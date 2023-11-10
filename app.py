63b10237e9ced2752b062c2d57cdb5f84ab8f198
avrians <avriansyahb10@gmail.com>
2023-11-10 11:01:33 +0700

    [Av][update riwayat artikel]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify,request
 import os,cv2
 from keras.models import Model,load_model
 # from keras.preprocessing.image import img_to_array
@@ -6,6 +6,8 @@ from tensorflow.keras.utils import img_to_array
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -13,6 +15,52 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 size=224
 
+app.secret_key = "Secrect Key"
+
+# Untuk menghubungkan ke database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:rootku@localhost/rumahtbc'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Model untuk tabel artikel kesehatan
+class ArtikelKesehatan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    judul = db.Column(db.String(100), nullable=False)
+    penulis = db.Column(db.String(100), nullable=False)
+    isi = db.Column(db.Text, nullable=False)
+    tanggal_publikasi = db.Column(db.Date, nullable=False)
+    kategori = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+    updated_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp(), nullable=False)
+    
+    def __init__(self, judul, penulis, isi, tanggal_publikasi, kategori):
+        self.judul = judul
+        self.penulis = penulis
+        self.isi = isi
+        self.tanggal_publikasi = tanggal_publikasi
+        self.kategori = kategori
+        
+# Model untuk tabel user
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nik = db.Column(db.String(16), unique=True, nullable=False) 
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, nik, username, password, email):
+        self.nik = nik
+        self.username = username
+        self.password = password
+        self.email = email
+
+# Untuk membuat tabel di database
+with app.app_context():
+    db.create_all()
+
+# Fungsi untuk memproses gambar
 def processimg(img):
     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (size, size)) 
@@ -21,22 +69,37 @@ def processimg(img):
     crop=cv2.resize(equalized_img,(size,size))
     return crop
 
+# Fungsi route untuk halaman index
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Fungsi route untuk halaman home
 @app.route('/home')
 def home():
     return render_template('dashboard_dokter.html')
 
+# Fungsi route untuk halaman deteksi dokter
 @app.route('/deteksi')
 def deteksi():
     return render_template('deteksi_dokter.html')
 
+# Fungsi route untuk halaman tentang user
 @app.route('/tentang')
 def tentang():
     return render_template('tentang.html')
 
+# Fungsi route untuk halaman riwayat user
+@app.route('/riwayatuser')
+def riwayatuser():
+    return render_template('riwayatuser.html')
+
+@app.route('/artikel')
+def artikel():
+    return render_template('artikel.html')
+
+
+# Fungsi route untuk memproses deteksi TBC dokter
 @app.route("/upload", methods=['POST'])
 def upload():
     model=load_model('modelTBC.h5')   
